86a4e6cdf373604705264d71586c0ad5ef512f73
Yilinliu <yilin.liu@scilifelab.se>
2023-04-19 11:41:52 +0200

    first version to track new files

---
+++ r_1st_step.R
@@ -0,0 +1,13 @@
+# comment 
+
+weight_kg <-  sqrt(10)
+weight_kg
+
+round(3.14, digits = 2)
+
+weight_g <- c(50,60,65,82)
+weight_g
+
+animals <- c('mouse', 'rat', 'dog')
+class(animals)
+class(weight_g)
